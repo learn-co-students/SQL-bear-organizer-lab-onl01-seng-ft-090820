1087f1573c1575311f1ab61c0df55e435b85e379
doosemavis <moosedavis2011@gmail.com>
2020-10-04 13:08:02 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) 
+VALUES (1, 'Mr. Chocolate', 20, 'male', 'brown', 'majestic', 'alive');
+
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) 
+VALUES (2, 'Rowdy', 10, 'male', 'black', 'calm', 'alive');
+
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) 
+VALUES (3, 'Tabitha', 6, 'female', 'brown', 'fierce', 'alive');
+
+INSERT INTO bears (id, name, age, gender, color, temperament, alive)
+VALUES (4, 'Segeant Brown', 3, 'male', 'brown', 'grumpy', 'dead');
+
+INSERT INTO bears (id, name, age, gender, color, temperament, alive)
+VALUES (5, 'Melissa', 13, 'female', 'white', 'chill', 'alive');
+
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) 
+VALUES (6, 'Grinch', 2, 'male', 'green', 'goofy', 'alive');
+
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) 
+VALUES (7, 'Wendy', 6, 'female', 'dark brown', 'bougie', 'alive');
+
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) 
+VALUES (8, NULL, 8, 'male', 'black', 'aggressive', 'dead');
