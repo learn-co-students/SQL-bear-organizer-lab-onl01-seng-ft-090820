47742f7d656d18be6660c43d7ffe4d091df38c7f
doosemavis <moosedavis2011@gmail.com>
2020-10-04 13:19:50 -0500

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,9 +1,9 @@
 def selects_all_female_bears_return_name_and_age
-  "Write your SQL query here"
+  ""
 end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
-  "Write your SQL query here"
+  "SELECT name FROM bears ORDER BY name ASC;"
 end
 
 def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
@@ -27,5 +27,5 @@ def counts_number_of_bears_with_goofy_temperaments
 end
 
 def selects_bear_that_killed_Tim
-  "Write your SQL query here"
+  "SELECT * FROM bears WHERE name IS NULL;"
 end
