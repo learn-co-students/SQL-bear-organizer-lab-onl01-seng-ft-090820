25da284b7e1ca200d50dd582229d4dec29a7d81f
cailinkless <cailin.nysff@gmail.com>
2020-10-08 16:36:07 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,17 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 52, "male", "brown", "sleepy", TRUE);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 18, "female", "red", "happy", TRUE);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 23, "nonbinary", "black", "dopey", TRUE);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 70, "male", "brown", "bashful", TRUE);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 5, "female", "red", "sneezy", TRUE);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 37, "male", "white", "grumpy", TRUE);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 12, "female", "purple", "doctorial", TRUE);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 30, "nonbinary", "black", "snow white", TRUE);
+
+-- Mr. Chocolate
+-- Rowdy
+-- Tabitha
+-- Sergeant Brown
+-- Melissa
+-- Grinch
+-- Wendy
+-- unnamed
