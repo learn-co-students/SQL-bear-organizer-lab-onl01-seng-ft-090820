626048b89c3c79673a1d7347e124c1d51d51f2f5
Travis Lee <travisslee223@gmail.com>
2020-10-08 02:29:50 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ( 321,'Mr.Chocolate', 3, 'male', 'brown', 'relaxed', 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ( 3231,'Rowdy', 3, 'male', 'grey', 'relaxed', 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ( 324321,'Tabitha', 3, 'female', 'brown', 'relaxed', 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ( 1,'Sergeant Brown', 3, 'male', 'black', 'relaxed', 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ( 41,'Melissa', 3, 'female', 'brown', 'relaxed', 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ( 61,'Grinch', 3, 'male', 'white', 'relaxed', 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ( 31,'Wenfy', 3, 'male', 'brown', 'relaxed', 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ( 91, Null, 3, 'male', 'brown', 'relaxed', 1);
