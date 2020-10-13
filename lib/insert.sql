82017e8d6eff4d0b46f7b258e934d8eb922486c7
Rocio Batres <jonasgirl21@gmail.com>
2020-10-13 05:10:44 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ('Mr. Chocolate','13','boy','brown','happy','true'); 
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ('Rowdy','10','fem boy','green','flirty','true');
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ('Tabitha','5','mom','yellow','nice','true');
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ('Sergeant Brown','16','old man','brown','strict','true');
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ('Melissa','2','girl','light pink','funny','true');
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ('Grinch','10','boy','green','grumpy','true');
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ('Wendy','8','queen','black','iconic','true');
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES (NULL,'13','boy','brown','not very nice','false');
